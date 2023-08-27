a2eead648c46a7f748e98b787443535fe32342f1
Pade Ramanaiah <ramanaiah.pade111@outlook.com>
2023-08-27 19:40:40 +0530

    commit

---
+++ src/test/java/com/ramana/jenkinsexample/JenkinsExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ramana.jenkinsexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JenkinsExampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
